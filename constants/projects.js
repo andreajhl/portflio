4e307ba953e70000d5f427684e0146cc93091d16
Andrea Hernandez <andreajhl29@gmail.com  git config --global user.name Andrea Hernandez>
2023-06-03 05:18:25 -0300

    add constants and icons

---
+++ constants/projects.js
@@ -0,0 +1,64 @@
+export const projectCard = {
+  mercadoLibre: '/assets/img/mercado-libre/meli.png',
+  eCommerce: '/assets/img/e-commerce/e-commerce.png',
+  famosos: '/assets/img/famosos/famosos.png',
+  nomad: '/assets/img/nomad/nomad.png',
+  ong: '/assets/img/ong/ong-1.png',
+};
+
+export const projectsCarousel = {
+  mercadoLibre: [
+    {
+      url: '/assets/img/mercado-libre/meli.png',
+      alt: 'banner market application shipments',
+    },
+  ],
+  eCommerce: [
+    {
+      url: '/assets/img/e-commerce/e-commerce.gif',
+      alt: 'video ecommerce of books',
+    },
+  ],
+  famosos: [
+    {
+      url: '/assets/img/famosos/famosos-1.png',
+      alt: 'celebrity homepage photo',
+    },
+    {
+      url: '/assets/img/famosos/famosos-2.png',
+      alt: 'photo purchase confirmation page',
+    },
+    {
+      url: '/assets/img/famosos/famosos-3.png',
+      alt: 'photo category page',
+    },
+  ],
+  nomad: [
+    {
+      url: '/assets/img/nomad/nomad-1.png',
+      alt: 'photo home page of the hostel',
+    },
+    {
+      url: '/assets/img/nomad/nomad-2.png',
+      alt: 'reservation section photo',
+    },
+    {
+      url: '/assets/img/nomad/nomad-3.png',
+      alt: 'photo section of venues',
+    },
+  ],
+  ong: [
+    {
+      url: '/assets/img/ong/ong-1.png',
+      alt: 'campaign page photo',
+    },
+    {
+      url: '/assets/img/ong/ong-2.png',
+      alt: 'photo login page',
+    },
+    {
+      url: '/assets/img/ong/ong-3.png',
+      alt: 'news page photo',
+    },
+  ],
+};
---
+++ public/assets/icons/index.js
@@ -0,0 +1,73 @@
+import { FaJira } from 'react-icons/fa'
+import { TbBrandNextjs } from 'react-icons/tb'
+import { DiGoogleAnalytics, DiScrum } from 'react-icons/di';
+import {
+  IoLogoNodejs,
+  IoLogoCss3,
+  IoLogoHtml5,
+} from 'react-icons/io';
+import {
+  SiJavascript,
+  SiTypescript,
+  SiNewrelic,
+  SiExpress,
+  SiReact,
+  SiTrello,
+  SiJest,
+  SiGithub,
+  SiBootstrap,
+  SiSlack,
+  SiSass,
+  SiChartdotjs,
+  SiRedux
+} from 'react-icons/si';
+
+const techIcons = [
+  <SiJavascript />,
+  <SiTypescript />,
+  <SiReact />,
+  <SiRedux />,
+  <TbBrandNextjs />,
+  <IoLogoNodejs/>,
+  <SiExpress />,
+  <SiJest />,
+  <IoLogoHtml5 />,
+  <IoLogoCss3 />,
+  <SiSass />,
+  <SiBootstrap />,
+  <SiChartdotjs />,
+  <DiGoogleAnalytics />,
+  <SiNewrelic />
+];
+
+const toolIcons = [
+  <SiGithub />,
+  <SiTrello />,
+  <FaJira />,
+  <DiScrum />,
+  <SiSlack />,
+];
+
+export {
+  SiJavascript,
+  SiTypescript,
+  SiNewrelic,
+  SiExpress,
+  SiReact,
+  SiTrello,
+  SiJest,
+  SiGithub,
+  SiBootstrap,
+  SiSlack,
+  SiSass,
+  SiChartdotjs,
+  IoLogoNodejs,
+  IoLogoCss3,
+  IoLogoHtml5,
+  DiGoogleAnalytics,
+  DiScrum,
+  TbBrandNextjs,
+  FaJira,
+  techIcons,
+  toolIcons,
+};

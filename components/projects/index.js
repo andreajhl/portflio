490dd910d4190a3240b3b593803092552c4c9929
Andrea Hernandez <andreajhl29@gmail.com  git config --global user.name Andrea Hernandez>
2023-06-23 04:00:01 -0300

    feat: :refactor: remove code

--- components/projects/index.js
+++ components/projects/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
 import { projectCard, projectsCarousel } from '@/constants/projects';
 import wordings from '@/wordings';
 import './styles.scss';

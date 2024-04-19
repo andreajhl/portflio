5b5cdb7cabf99810e283e981dc48f3defb6e1942
Andrea Hernandez <andreajhl29@gmail.com>
2024-04-19 11:03:49 -0300

    add translate

---
+++ i18n/translates/globals/en.js
@@ -0,0 +1,64 @@
+const wordings = {
+  about: {
+    title: 'Acerca de mí',
+    content: [
+      'Soy Andrea Hernandéz, {{profession}} con 3 años de experiencia. Me apasiona crear interfaces interactivas y atractivas.',
+      'Convierto diseños creatvos en código limpio y eficiente utilizando HTML, CDD y Javascript, junto con frameworks como React y Next.js. Tengo experiencia en la integración de APIs y la optimizacion del rendimiento en sitios webs.',
+      'Siempre busco nuevas oportunidades para crecer y mantenerme actualizada. Me considero una comunicadora efectiva y disfruto colaborar en equipos dinámicos y mutidisciplinarios.'
+    ],
+    options: {
+      contact: 'Contactame',
+      viewProject: 'Ver proyectos',
+    }
+  },
+  navbar: {
+    initial: 'Inicio',
+    about: 'Acerca de mí',
+    skill: 'Skill',
+    projects: 'Proyectos',
+    cv: 'Currículum',
+    contact: 'Contacto'
+  },
+  home: {
+    title: '!Hola! Soy Andrea',
+    subtitle: 'Ssr. Front End Developer',
+    button: 'Currículum'
+  },
+  skill: {
+    technology: 'Tecnología',
+    tools: 'Herramientas'
+  },
+  projects: {
+    title: 'Ultimos Proyectos',
+    button: 'Leer más',
+    Ficohsa: {
+      title: 'Fichosa',
+      subtitle: 'Grupo Financiero',
+      stackTechnology: {
+        title: 'Tecnologías principales',
+        stack: 'NextJs, TS, Webpack'
+      },
+      description: 'Desarrollo de una biblioteca de componentes transversales para los demás squads y creación de componentes funcionales (micro front ends) fundamentales para optimizar el tiempo de nuevos desarrollos y mantener la consistencia en el desarrollo.',
+    },
+    meli: {
+      title: 'Mercado Libre',
+      subtitle: 'E-commerce',
+      stackTechnology: {
+        title: 'Tecnologías principales',
+        stack: 'React, NextJs, JS, TS, Chart.js',
+      },
+      description: 'Desarrollo de un sistema de logistica para analisis y creacion de informes mediante el uso de tarjetas, tablas y graficas, permitiendo a las operaciones tomar acciones estrategicas para alcanzar los objetivos establecidos.',
+    },
+    Famosos: {
+      title: 'Famosos.com',
+      subtitle: 'E-commerce',
+      stackTechnology: {
+        title: 'Tecnologías principales',
+        stack: 'Nextjs, React, TS, JS'
+      },
+      description: 'La plataforma conecta usuarios con celebridades de America Latina, permitiendo adquirir videos personalizados o autografos digitales, directamente en su correo electronico,',
+    },
+  }
+};
+
+export default wordings;
